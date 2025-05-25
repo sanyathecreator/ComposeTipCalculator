d20aadc17d8242f4d9450af26d750fe1cacebaef
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-05-25 20:59:55 +0300

    Add basic UI.

--- app/src/main/java/com/example/composetipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/composetipcalculator/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetipcalculator.ui.theme.ComposeTipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,5 +39,61 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipCalculatorApp() {
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Calculate tip"
+        )
+        EditNumberField(
+            value = "",
+            onValueChange = {},
+            label = "Bill amount",
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+        EditNumberField(
+            value = "",
+            onValueChange = {},
+            label = "Tip percentage",
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+        Text(
+            text = "Tip amount: $0.00",
+            style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier.padding(vertical = 8.dp)
 
+        )
+    }
+}
+
+@Composable
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+) {
+    TextField(
+        value = value,
+        singleLine = true,
+        modifier = modifier,
+        onValueChange = onValueChange,
+        label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TipCalculatorAppPreview() {
+    ComposeTipCalculatorTheme {
+        TipCalculatorApp()
+    }
 }
